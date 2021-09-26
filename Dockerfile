ac867d84d2123bcd635eccf379356a25f89b6a8e
Robert Diers <robertdiers@gmail.com>
2021-09-26 19:48:58 +0200

    removed timer

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,16 @@
-#FROM debian:stable-slim
-#FROM ubuntu:impish
 FROM python:slim-buster
 
 USER root
 
 RUN apt update
 RUN apt -y upgrade
-RUN pip3 install configparser
-RUN pip3 install flask
-RUN pip3 install apscheduler
+RUN pip3 install configparser flask
 
 # copy files
 COPY clickdelay.py /app/clickdelay.py
 RUN chmod u+x /app/clickdelay.py
 COPY clickdelay.ini /app/clickdelay.ini
 COPY html /app/html
-#COPY timer1.py /app/timer1.py
-#RUN chmod u+x /app/timer1.py
-#COPY timer1.ini /app/timer1.ini
 
 # set workdir
 WORKDIR /app
--- clickdelay.py
+++ clickdelay.py
@@ -5,7 +5,6 @@ from threading import Timer
 from urllib.request import urlopen
 from datetime import datetime
 import configparser
-from apscheduler.schedulers.background import BackgroundScheduler
 
 #read config
 config = configparser.ConfigParser()
@@ -58,42 +57,6 @@ def color(tasmota):
         return '#DAF7A6'
     else:
         return '#FF5733'
-
-def checktimer():
-    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' ' + ' timer1 check: ' + config['TimerSection']['tasmota.status'])
-    currentMonth = datetime.now().month
-    currentHour = datetime.now().hour
-    activation = config['TimerSection']['timer.'+str(currentMonth)]
-    fromTo = activation.split("-")
-    #activation during night
-    if currentHour >= int(fromTo[0]) or currentHour <= int(fromTo[1]):
-        timeractivate()
-        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' ' + 'timer1 activated')
-    else:
-        timerdeactivate()
-        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' ' + 'timer1 deactivated')
-    return
-
-def timeractivate():
-    statuslink = urlopen(config['TimerSection']['tasmota.status'])
-    tasmotastatus = statuslink.read().decode('utf-8')
-    if 'OFF' in tasmotastatus:
-        switchlink = urlopen(config['TimerSection']['tasmota.switch'])    
-        retval = switchlink.read().decode('utf-8')
-        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' ' + retval)
-
-def timerdeactivate():
-    statuslink = urlopen(config['TimerSection']['tasmota.status'])
-    tasmotastatus = statuslink.read().decode('utf-8')
-    if 'ON' in tasmotastatus:
-        switchlink = urlopen(config['TimerSection']['tasmota.switch'])    
-        retval = switchlink.read().decode('utf-8')
-        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' ' + retval)
-
-sched = BackgroundScheduler(daemon=True)
-sched.add_job(checktimer,'interval', minutes=1)
-sched.start()
-print(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' timer1 created every minute')
 
 #start at port 50000
 app.run(host='0.0.0.0', port=50000)
